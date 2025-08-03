59578a2819bb6d29fbd816785cbf14be0a659766
Muhammad Hasan <160401271+hn8378429@users.noreply.github.com>
2025-08-04 00:47:16 +0500

    header.tsx

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
 import Link from "next/link";
 
 export default function Header() {
+  useEffect(() => {
+    const toggleOpen = document.getElementById("toggleOpen");
+    const toggleClose = document.getElementById("toggleClose");
+    const collapseMenu = document.getElementById("collapseMenu");
+
+    if (toggleOpen && collapseMenu) {
+      toggleOpen.addEventListener("click", () => {
+        collapseMenu.classList.add("!block");
+      });
+    }
+
+    if (toggleClose && collapseMenu) {
+      toggleClose.addEventListener("click", () => {
+        collapseMenu.classList.remove("!block");
+      });
+    }
+  }, []);
+
   return (
     <header className="flex border-b py-3 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
       <div className="flex flex-wrap items-center gap-5 w-full">
@@ -25,12 +46,8 @@ export default function Header() {
               className="w-3.5 h-3.5 fill-black"
               viewBox="0 0 320.591 320.591"
             >
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-              ></path>
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-              ></path>
+              <path d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z" />
+              <path d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z" />
             </svg>
           </button>
 
